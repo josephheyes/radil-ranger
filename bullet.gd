726f09fb7c5c6a5412109495f1a334b708c10013
Isaac Destura <isaacdestura@gmail.com>
2024-11-03 01:00:04 +0000

    feat: add bullets colliding

--- bullet.gd
+++ bullet.gd
@@ -4,6 +4,7 @@ const SPEED = 40.0
 
 @onready var mesh = $MeshInstance3D
 @onready var ray = $RayCast3D
+@onready var particles = $GPUParticles3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,3 +14,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position += transform.basis * Vector3(0, 0, -SPEED) * delta
+	if ray.is_colliding():
+		mesh.visible = false
+		particles.emitting = true
+		await get_tree().create_timer(1.0).timeout
+		queue_free()
+		
+func _on_timer_timeout():
+	queue_free()
