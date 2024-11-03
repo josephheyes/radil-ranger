23664ee329cd54bb777c3d77a9a1c90de40d0aa7
Isaac Destura <isaacdestura@gmail.com>
2024-11-03 04:33:30 +0000

    check target type and add prints

--- bullet.gd
+++ bullet.gd
@@ -6,6 +6,11 @@ const SPEED = 40.0
 @onready var ray = $RayCast3D
 @onready var particles = $GPUParticles3D
 
+@export var damage := 1
+var health = 6
+
+signal body_part_hit(dam)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -17,8 +22,21 @@ func _process(delta: float) -> void:
 	if ray.is_colliding():
 		mesh.visible = false
 		particles.emitting = true
+		ray.enabled = true
+		if !ray.get_collider() is StaticBody3D:
+			ray.get_collider().hit()
 		await get_tree().create_timer(1.0).timeout
 		queue_free()
 		
 func _on_timer_timeout():
 	queue_free()
+	
+func hit():
+	print("test")
+	emit_signal("body_part_hit", damage)
+	
+func _on_area_3d_body_part_hit(dam):
+	print(health)
+	health -= dam
+	if health <= 0:
+		queue_free()
